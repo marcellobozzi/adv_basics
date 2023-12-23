e43557a6739b980fd49399f73b042c80fabaa4c3
Marcello Bozzi <marcello.bozzi@itismajo.onmicrosoft.com>
2023-12-23 16:27:27 +0100

    start screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
 
 void main() {
-  runApp(const LearnFlutterApp());
-}
-
-class LearnFlutterApp extends StatelessWidget {
-  const LearnFlutterApp({super.key});
-
-  @override
-  Widget build(context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 200,
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print('start quiz pressed');
-                },
-                child: const Text('Start Quiz!'),
-              ),
-            ],
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.purple],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
+          child: const StartScreen(),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 24,
+              color: Color.fromARGB(255, 237, 223, 252),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {
+              print('start quiz pressed');
+            },
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz!'),
+          ),
+        ],
+      ),
+    );
+  }
+}
